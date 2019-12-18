d8240e79da8962b7143f65e51dcd34c841f58ec9
EliasJRH <58529029+EliasJRH@users.noreply.github.com>
2019-12-18 14:08:49 -0500

    update

--- notALOZRipOff/src/notalozripoff/ghostClass.java
+++ notALOZRipOff/src/notalozripoff/ghostClass.java
@@ -19,18 +19,20 @@ import javax.swing.Timer;
  *
  * @author s241556
  */
-public class ghostClass extends JPanel implements ActionListener{
-    
-    Timer hitDetection = new Timer(1, this);
+public class ghostClass extends JPanel implements ActionListener {
 
-    public ghostClass(){
+    Timer hitDetection = new Timer(1, this);
+    static int damage = 5;
+    static int health = 10;
+    static boolean hitL = false, hitR = false, hitU = false, hitD = false;
+
+    public ghostClass() {
         hitDetection.start();
-        setSize(128,128);
-        setOpaque(false); 
-        int damage = 5;
-        int health = 1;
+        setSize(128, 128);
+        setOpaque(false);
+
     }
-        
+
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         ImageIcon GHOST = new ImageIcon("C:\\Users\\s241556\\Documents\\NetBeansProjects\\notALOZRipOff\\areaImages\\Ghost.gif");
@@ -45,27 +47,55 @@ public class ghostClass extends JPanel implements ActionListener{
         boolean listenForHitD = classForTesting.attackD;
         if (listenForHitR) {
             if ((classForTesting.xpos + 100 >= (this.getLocation().x)) && ((classForTesting.xpos + 100 < (this.getLocation().x + 75)))) { //Attack right to left
-                JOptionPane.showMessageDialog(null, "hit 1");
+                health -= classForTesting.strength;
+                this.setLocation(this.getLocation().x - 25, this.getLocation().y);
             }
         } else if (listenForHitL) {
             if (((classForTesting.xpos) <= (this.getLocation().x + this.getWidth())) && (classForTesting.xpos >= (this.getLocation().x + this.getWidth() - 75))) { //attack left to right
-                JOptionPane.showMessageDialog(null, "hit 2");
+                health -= classForTesting.strength;
+                this.setLocation(this.getLocation().x + 25, this.getLocation().y);
             }
         } else if (listenForHitU) {
             if (classForTesting.ypos <= (this.getLocation().y + this.getHeight()) && (classForTesting.ypos >= (this.getLocation().y + this.getHeight() - 75))) {
-                JOptionPane.showMessageDialog(null, "hit 3");
+                health -= classForTesting.strength;
+                this.setLocation(this.getLocation().x, this.getLocation().y - 25);
             }
         } else if (listenForHitD) {
             if ((classForTesting.ypos + 200) >= (this.getLocation().y) && (classForTesting.ypos + 192 <= (this.getLocation().y + 75))) {
-                JOptionPane.showMessageDialog(null, "hit 4");
+                health -= classForTesting.strength;
+                this.setLocation(this.getLocation().x, this.getLocation().y + 25);
+            }
+        } else {
+            if ((classForTesting.xpos <= (this.getLocation().x + this.getWidth()))) { //Attack right to left
+                classForTesting.health -= damage;
+                hitL = true;
+            } else if (((classForTesting.xpos + 100) == (this.getLocation().x))) { //attack left to right
+                classForTesting.health -= damage;
+                JOptionPane.showMessageDialog(null, classForTesting.xpos + " " + this.getLocation().x);
+                hitR = true;
+            } else if (classForTesting.ypos == (this.getLocation().y + this.getHeight())) {
+                classForTesting.health -= damage;
+                hitU = true;
+                JOptionPane.showMessageDialog(null, classForTesting.ypos + " 1" + this.getLocation().y);
+            } else if ((classForTesting.ypos + 192) == (this.getLocation().y)) {
+                classForTesting.health -= damage;
+                hitD = true;
+                JOptionPane.showMessageDialog(null, classForTesting.ypos + " 2" + this.getLocation().y);
             }
         }
         listenForHitR = false;
         listenForHitL = false;
         listenForHitU = false;
         listenForHitD = false;
-        /*
-        int randomxpos = (int) (Math.random() * (1250 - 50) + 1) + 50;
+        if (health <= 0) {
+            this.setVisible(false);
+            this.setEnabled(false);
+        } else {
+            this.setVisible(true);
+            this.setEnabled(true);
+        }
+        if (classForTesting.detectChange){
+            int randomxpos = (int) (Math.random() * (1250 - 50) + 1) + 50;
             int randomypos = (int) (Math.random() * (750 - 50) + 1) + 50;
         if (classForTesting.areaBooleans[2][1]) {
                 this.setLocation(randomxpos, randomypos);
@@ -126,5 +156,6 @@ public class ghostClass extends JPanel implements ActionListener{
             } else if (classForTesting.areaBooleans[4][4]) {
                 this.setLocation(randomxpos, randomypos);
             }
-        */
+        }
+    }
 }
