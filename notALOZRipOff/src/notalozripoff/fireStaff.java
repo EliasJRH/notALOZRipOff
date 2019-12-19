b37df240ac60627b7329fe9b00f163845d76c441
IceHawk246 <45637919+IceHawk246@users.noreply.github.com>
2019-12-19 17:01:22 -0500

    Update fireStaff.java

--- notALOZRipOff/src/notalozripoff/fireStaff.java
+++ notALOZRipOff/src/notalozripoff/fireStaff.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package notalozripoff;
 
 import java.awt.Graphics;
@@ -21,25 +17,25 @@ public class fireStaff extends JPanel implements ActionListener {
     Timer placementTimer = new Timer(5, this);
 
     public fireStaff() {
-        placementTimer.start();
-        setVisible(false);
+        placementTimer.start(); //Constructs default values for this objects
+        setVisible(false);      //For timer, visibility, and size
         setSize(128, 128);
 
     }
 
-    public void paintComponent(Graphics g1) {
+    public void paintComponent(Graphics g1) { //Method used to set up the image of the fire staff
         super.paintComponent(g1);
         ImageIcon fireStaffI = new ImageIcon("C:\\Users\\s241556\\Documents\\NetBeansProjects\\notALOZRipOff\\areaImages\\fireStaff.png");
         fireStaffI.paintIcon(this, g1, 0, 0);
     }
 
     @Override
-    public void actionPerformed(ActionEvent e) {
-        if (classForTesting.areaBooleans[0][0]) {
-            this.setVisible(true);
+    public void actionPerformed(ActionEvent e) { 
+        if (classForTesting.areaBooleans[0][0]) { //When detecting that it is in tile 0:0, it will set 
+            this.setVisible(true);                //its location and visibility true
             this.setLocation(50,50);
         } else {
-            this.setVisible(false);
+            this.setVisible(false); //Otherwise it will remain not visible
         }
     }
 
