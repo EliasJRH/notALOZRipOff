dbe1257ce6d8824574f51942345133d5a0890b5d
IceHawk246 <45637919+IceHawk246@users.noreply.github.com>
2019-12-19 17:15:03 -0500

    Update purpleStaff.java

--- notALOZRipOff/src/notalozripoff/purpleStaff.java
+++ notALOZRipOff/src/notalozripoff/purpleStaff.java
@@ -10,13 +10,14 @@ public class purpleStaff extends JPanel implements ActionListener {
 
     Timer placementTimer = new Timer(5, this);
     
-    public purpleStaff() {
+    public purpleStaff() {          //Constructor with default values of the object
         placementTimer.start();
         setVisible(false);
         setSize(128, 128);
+        setOpaque(false);
     }
     
-    public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) { //Method used to set up the image of the purple staff
         super.paintComponent(g);
         ImageIcon purpleStaffI = new ImageIcon("pathway for the Adrian's rock picture");
         purpleStaffI.paintIcon(this, g, 0, 0);
@@ -24,11 +25,11 @@ public class purpleStaff extends JPanel implements ActionListener {
     
     @Override
     public void actionPerformed(ActionEvent e) {
-        if (classForTesting.areaBooleans[4][4]) {
-            this.setVisible(true);
-            this.setLocation(750, 750); //Location Right here needs to be changed
+        if (classForTesting.areaBooleans[4][4]) { //When the user is at tile 4:4, then it will set 
+            this.setVisible(true);                //the purple staff's location and visibility to false
+            this.setLocation(750, 750);
         } else {
-            this.setVisible(false);
+            this.setVisible(false); //If the user is detected elsewhere, then it will remain not visible
         }
     }
     
