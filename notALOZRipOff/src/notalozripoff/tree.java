ec0f867fdee326ea903ee739addd0f5e32c8ba0d
EliasJRH <58529029+EliasJRH@users.noreply.github.com>
2019-12-20 12:59:34 -0500

    Adrian to comment

--- notALOZRipOff/src/notalozripoff/tree.java
+++ notALOZRipOff/src/notalozripoff/tree.java
@@ -40,8 +40,7 @@ public class tree extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        boolean findChange = classForTesting.detectChange;
-        if (findChange) {
+        if (classForTesting.detectChange) {
             classForTesting.detectChange = false;
             if (classForTesting.areaBooleans[2][1]) {
                 this.setLocation(500, 250);
@@ -49,69 +48,56 @@ public class tree extends JPanel implements ActionListener {
                 this.setLocation(700, 250);
             } else if (classForTesting.areaBooleans[2][2]) {
                 this.setLocation(500, 500);
+            } else if (classForTesting.areaBooleans[2][0]) {
+                this.setLocation(895, 23);
+            } else if (classForTesting.areaBooleans[2][4]) {
+                this.setLocation(1377, 459);
+            } //------------------------------------------- Row 0
+            else if (classForTesting.areaBooleans[0][1]) {
+                this.setLocation(1072, 679);
+            } else if (classForTesting.areaBooleans[0][0]) {
+                this.setLocation(616, 614);
+            } else if (classForTesting.areaBooleans[0][2]) {
+                this.setLocation(443, 259);
+            } else if (classForTesting.areaBooleans[0][3]) {
+                this.setLocation(101, 63);
+            } else if (classForTesting.areaBooleans[0][4]) {
+                this.setLocation(858, 207);
+            } //------------------------------------------- Row 1
+            else if (classForTesting.areaBooleans[1][0]) {
+                this.setLocation(872, 721);
+            } else if (classForTesting.areaBooleans[1][1]) {
+                this.setLocation(1006, 287);
+            } else if (classForTesting.areaBooleans[1][2]) {
+                this.setLocation(773, 444);
+            } else if (classForTesting.areaBooleans[1][3]) {
+                this.setLocation(260, 332);
+            } else if (classForTesting.areaBooleans[1][4]) {
+                this.setLocation(665, 464);
+            } //------------------------------------------- Row 3
+            else if (classForTesting.areaBooleans[3][0]) {
+                this.setLocation(1022, 251);
+            } else if (classForTesting.areaBooleans[3][1]) {
+                this.setLocation(765, 100);
+            } else if (classForTesting.areaBooleans[3][2]) {
+                this.setLocation(1050, 500);
+            } else if (classForTesting.areaBooleans[3][3]) {
+                this.setLocation(800, 655);
+            } else if (classForTesting.areaBooleans[3][4]) {
+                this.setLocation(365, 463);
+            } //------------------------------------------- Row 4
+            else if (classForTesting.areaBooleans[4][0]) {
+                this.setLocation(605, 698);
+            } else if (classForTesting.areaBooleans[4][1]) {
+                this.setLocation(752, 652);
+            } else if (classForTesting.areaBooleans[4][2]) {
+                this.setLocation(156, 340);
+            } else if (classForTesting.areaBooleans[4][3]) {
+                this.setLocation(750, 478);
+            } else if (classForTesting.areaBooleans[4][4]) {
+                this.setLocation(354, 247);
             }
 
         }
-/*
-if (classForTesting.areaBooleans[2][1]) {
-                this.setLocation(500, 250);
-            } else if (classForTesting.areaBooleans[2][3]) {
-                this.setLocation(700, 250);
-            } else if (classForTesting.areaBooleans[2][2]) {
-                this.setLocation(500, 500);
-            } else if (classForTesting.areaBooleans[2][0]) {
-            	this.setLocation(895, 23);
-            } else if (classForTesting.areaBooleans[2][4]) {
-            	this.setLocation(1377, 459);
-            }
-            //------------------------------------------- Row 0
-            else if (classForTesting.areaBooleans[0][1]) {
-            	this.setLocation(1072, 679);
-            } else if (classForTesting.areaBooleans[0][0]) {
-            	this.setLocation(616, 614);
-            } else if (classForTesting.areaBooleans[0][2]) {
-            	this.setLocation(443, 259);
-            } else if (classForTesting.areaBooleans[0][3]) {
-            	this.setLocation(101, 63);
-            } else if (classForTesting.areaBooleans[0][4]) {
-            	this.setLocation(858, 207);
-            }
-            //------------------------------------------- Row 1
-            else if (classForTesting.areaBooleans[1][0]) {
-            	this.setLocation(872, 721);
-            } else if (classForTesting.areaBooleans[1][1]) {
-            	this.setLocation(1006, 287);
-            } else if (classForTesting.areaBooleans[1][2]) {
-            	this.setLocation(773, 444);
-            } else if (classForTesting.areaBooleans[1][3]) {
-            	this.setLocation(260, 332);
-            } else if (classForTesting.areaBooleans[1][4]) {
-            	this.setLocation(665, 464);
-            }
-            //------------------------------------------- Row 3
-            else if (classForTesting.areaBooleans[3][0]) {
-            	this.setLocation(1022, 251);
-            } else if (classForTesting.areaBooleans[3][1]) {
-            	this.setLocation(765, 100);
-            } else if (classForTesting.areaBooleans[3][2]) {
-            	this.setLocation(1050, 500);
-            } else if (classForTesting.areaBooleans[3][3]) {
-            	this.setLocation(800, 655);
-            } else if (classForTesting.areaBooleans[3][4]) {
-            	this.setLocation(365, 463);
-            }
-            //------------------------------------------- Row 4
-            else if (classForTesting.areaBooleans[4][0]) {
-            	this.setLocation(605, 698);
-            } else if (classForTesting.areaBooleans[4][1]) {
-            	this.setLocation(752, 652);
-            } else if (classForTesting.areaBooleans[4][2]) {
-            	this.setLocation(156, 340);
-            } else if (classForTesting.areaBooleans[4][3]) {
-            	this.setLocation(750, 478);
-            } else if (classForTesting.areaBooleans[4][4]) {
-            	this.setLocation(354, 247);
-            }
-*/
     }
 }
