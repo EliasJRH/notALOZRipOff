a3b894a2bb40d836cfa2ce153c356ac7d2a6eff5
IceHawk246 <45637919+IceHawk246@users.noreply.github.com>
2019-12-20 14:02:27 -0500

    added comments for slime class (Shawn)

--- notALOZRipOff/src/notalozripoff/slimeClass.java
+++ notALOZRipOff/src/notalozripoff/slimeClass.java
@@ -22,14 +22,14 @@ public class slimeClass extends JPanel implements ActionListener {
     static int health = 50;
     boolean upgradeGiven = false;
 
-    public slimeClass() {
-        placementTimer.start();
-        setSize(227, 234);      //Will change size later
+    public slimeClass() {       //Constructor method used to set up the default values of the slime object
+        placementTimer.start(); //Such as placement timer, location, size and opacity
+        setSize(227, 234);      
         setLocation(500, 500);
         setOpaque(false);
     }
 
-    public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) {   //Method used to set up the image of the slime object
         super.paintComponent(g);
         ImageIcon SLIME = new ImageIcon("C:\\Users\\s241556\\Documents\\NetBeansProjects\\notALOZRipOff\\areaImages\\Slime.png");
         SLIME.paintIcon(this, g, 0, 0);
@@ -37,51 +37,55 @@ public class slimeClass extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        boolean listenForHitR = classForTesting.attackR;
-        boolean listenForHitL = classForTesting.attackL;
-        boolean listenForHitU = classForTesting.attackU;
-        boolean listenForHitD = classForTesting.attackD;
-        if (listenForHitR) {
+        boolean listenForHitR = classForTesting.attackR;//Booleans that receive the value of where the user is hitting it from
+        boolean listenForHitL = classForTesting.attackL;  
+        boolean listenForHitU = classForTesting.attackU; //Ex. User hitting ghost from left to right = ghost receiving hit
+        boolean listenForHitD = classForTesting.attackD;    //On the left side
+        if (listenForHitR) {            //When hitting it from the the left to right, it moves 25 units to the right
+                                //Second if statement checks if it's in bound 
             if ((classForTesting.xpos + 100 >= (this.getLocation().x)) && ((classForTesting.xpos + 100 < (this.getLocation().x + 75)))) { //Attack right to left
                 health -= classForTesting.strength;
                 this.setLocation(this.getLocation().x + 25, this.getLocation().y);
             }
-        } else if (listenForHitL) {
+        } else if (listenForHitL) {     //When hitting it on the ghost from right to left, it moves 25 units to the left
+            //Second if statement checks if it's in bound
             if (((classForTesting.xpos) <= (this.getLocation().x + this.getWidth())) && (classForTesting.xpos >= (this.getLocation().x + this.getWidth() - 75))) { //attack left to right
                 health -= classForTesting.strength;
                 this.setLocation(this.getLocation().x - 25, this.getLocation().y);
             }
-        } else if (listenForHitU) {
+        } else if (listenForHitU) {     //When hitting the ghost from bottom to up, it moves 25 units to the up
+            //Second if statement checks if it's in bound
             if (classForTesting.ypos <= (this.getLocation().y + this.getHeight()) && (classForTesting.ypos >= (this.getLocation().y + this.getHeight() - 75))) {
                 health -= classForTesting.strength;
                 this.setLocation(this.getLocation().x, this.getLocation().y - 25);
             }
-        } else if (listenForHitD) {
+        } else if (listenForHitD) {     //When hitting the ghost from top to down, it moves 25 units to the down
+            //Second if statement checks if it's in bound
             if ((classForTesting.ypos + 200) >= (this.getLocation().y) && (classForTesting.ypos + 192 <= (this.getLocation().y + 75))) {
                 health -= classForTesting.strength;
                 this.setLocation(this.getLocation().x, this.getLocation().y + 25);
             }
         }
-        listenForHitR = false;
+        listenForHitR = false;      //Sets all of the booleans mentioned above false
         listenForHitL = false;
         listenForHitU = false;
         listenForHitD = false;
         
         if (health <= 0) {
-            upgradeGiven = true;
-            this.setVisible(false);
+            upgradeGiven = true;    
+            this.setVisible(false); //When detecting that it's health is gone, then it will be set not visible
             this.setEnabled(false);
             classForTesting.enemiesKilled++;
         } else {
-            this.setVisible(true);
+            this.setVisible(true);  //Else it will be left visible 
             this.setEnabled(true);
         }
-        if (classForTesting.detectChange3) {
-            classForTesting.detectChange3 = false;
+        if (classForTesting.detectChange3) {        //Checks the position of where it is
+            classForTesting.detectChange3 = false;      //First if statement detects if it changed tiles on the map
             int randomxpos = (int) (Math.random() * (1250 - 50) + 1) + 50;
-            int randomypos = (int) (Math.random() * (750 - 50) + 1) + 50;
+            int randomypos = (int) (Math.random() * (750 - 50) + 1) + 50; //Then second if statement changes the location of the ghost object randomly
             if (classForTesting.areaBooleans[2][1]) {       // Row 2
-                this.setLocation(randomxpos, randomypos);
+                this.setLocation(randomxpos, randomypos);       
             } else if (classForTesting.areaBooleans[2][3]) {
                 this.setLocation(randomxpos, randomypos);
             } else if (classForTesting.areaBooleans[2][2]) {
