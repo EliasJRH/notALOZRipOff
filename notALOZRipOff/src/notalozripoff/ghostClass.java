fdd6caac02b115d63af8449ec21a1249fa9c0bd5
IceHawk246 <45637919+IceHawk246@users.noreply.github.com>
2019-12-20 13:54:29 -0500

    added in comments

--- notALOZRipOff/src/notalozripoff/ghostClass.java
+++ notALOZRipOff/src/notalozripoff/ghostClass.java
@@ -5,12 +5,12 @@
  */
 package notalozripoff;
 
-import java.awt.Graphics;
+import java.awt.Graphics;           //All the imports needed to be used, except for logger
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.ImageIcon;
+import javax.swing.ImageIcon;   
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.Timer;
@@ -21,15 +21,15 @@ import javax.swing.Timer;
  */
 public class ghostClass extends JPanel implements ActionListener {
 
-    Timer hitDetection = new Timer(5, this);
-    static int damage = 5;
+    Timer hitDetection = new Timer(5, this);   //Variables to be used in this class and when referencing
+    static int damage = 5;                      //In other classes
     static int health = 30;
     int xpos = 100;
     int ypos = 500;
     static boolean hitL = false, hitR = false, hitU = false, hitD = false;
 
-    public ghostClass() {
-        hitDetection.start();
+    public ghostClass() {       //Constructor that sets up the default values of the ghost object
+        hitDetection.start();   //Such as the timer to detect being hit, location, opacity, and visibility
         setSize(128, 128);
         setLocation(100, 500);
         setOpaque(false);
@@ -40,7 +40,7 @@ public class ghostClass extends JPanel implements ActionListener {
         }
     }
 
-    public void paintComponent(Graphics g) {
+    public void paintComponent(Graphics g) { //Method used to set up the image of the ghost
         super.paintComponent(g);
         ImageIcon GHOST = new ImageIcon("C:\\Users\\s241556\\Documents\\NetBeansProjects\\notALOZRipOff\\areaImages\\Ghost.gif");
         GHOST.paintIcon(this, g, 0, 0);
@@ -48,47 +48,51 @@ public class ghostClass extends JPanel implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
-        boolean listenForHitR = classForTesting.attackR;
-        boolean listenForHitL = classForTesting.attackL;
-        boolean listenForHitU = classForTesting.attackU;
-        boolean listenForHitD = classForTesting.attackD;
-        if (listenForHitR) {
+        boolean listenForHitR = classForTesting.attackR;//Booleans that receive the value of where the user is hitting it from
+        boolean listenForHitL = classForTesting.attackL;  
+        boolean listenForHitU = classForTesting.attackU; //Ex. User hitting ghost from left to right = ghost receiving hit
+        boolean listenForHitD = classForTesting.attackD;    //On the left side
+        if (listenForHitR) {    //When hitting it from the the left to right, it moves 25 units to the right
+                                //Second if statement checks if it's in bound 
             if ((classForTesting.xpos + 100 >= (this.getLocation().x)) && ((classForTesting.xpos + 100 < (this.getLocation().x + 75)))) { //Attack right to left
-                health -= classForTesting.strength;
+                health -= classForTesting.strength;//loses health based on how much damage it received from the player
                 this.setLocation(this.getLocation().x + 25, this.getLocation().y);
             }
-        } else if (listenForHitL) {
+        } else if (listenForHitL) {     //When hitting it on the ghost from right to left, it moves 25 units to the left
+            //Second if statement checks if it's in bound 
             if (((classForTesting.xpos) <= (this.getLocation().x + this.getWidth())) && (classForTesting.xpos >= (this.getLocation().x + this.getWidth() - 75))) { //attack left to right
-                health -= classForTesting.strength;
+                health -= classForTesting.strength;     //loses health based on how much damage it received from the player
                 this.setLocation(this.getLocation().x - 25, this.getLocation().y);
             }
-        } else if (listenForHitU) {
+        } else if (listenForHitU) {   //When hitting the ghost from bottom to up, it moves 25 units to the up
+            //Second if statement checks if it's in bound
             if (classForTesting.ypos <= (this.getLocation().y + this.getHeight()) && (classForTesting.ypos >= (this.getLocation().y + this.getHeight() - 75))) {
-                health -= classForTesting.strength;
+                health -= classForTesting.strength;     //loses health based on how much damage it received from the player
                 this.setLocation(this.getLocation().x, this.getLocation().y - 25);
             }
-        } else if (listenForHitD) {
+        } else if (listenForHitD) {//When hitting the ghost from top to down, it moves 25 units to the down
+            //Second if statement checks if it's in bound
             if ((classForTesting.ypos + 200) >= (this.getLocation().y) && (classForTesting.ypos + 192 <= (this.getLocation().y + 75))) {
-                health -= classForTesting.strength;
+                health -= classForTesting.strength;     //loses health based on how much damage it received from the player
                 this.setLocation(this.getLocation().x, this.getLocation().y + 25);
             }
         }
-        listenForHitR = false;
+        listenForHitR = false;  //Sets all of the booleans mentioned above false
         listenForHitL = false;
         listenForHitU = false;
         listenForHitD = false;
-        if (health <= 0) {
-            this.setVisible(false);
-            this.setEnabled(false);
+        if (health <= 0) {          //When detected that the ghost's health is gone, it will set it not visible
+            this.setVisible(false); //And disable  from being enabled
+            this.setEnabled(false); //Else it will be left visible
         } else {
             this.setVisible(true);
             this.setEnabled(true);
         }
-        if (classForTesting.detectChange) {
-            classForTesting.detectChange = false;
-            int randomxpos = (int) (Math.random() * (1250 - 50) + 1) + 50;
+        if (classForTesting.detectChange) {     //Checks the position of where it is
+            classForTesting.detectChange = false;   //First if statement detects if it changed tiles on the map
+            int randomxpos = (int) (Math.random() * (1250 - 50) + 1) + 50;  
             int randomypos = (int) (Math.random() * (750 - 50) + 1) + 50;
-            if (classForTesting.areaBooleans[2][1]) {
+            if (classForTesting.areaBooleans[2][1]) {       //Then second if statement changes the location of the ghost object randomly
                 this.setLocation(randomxpos, randomypos);
             } else if (classForTesting.areaBooleans[2][3]) {
                 this.setLocation(randomxpos, randomypos);
